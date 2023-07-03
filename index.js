710f96540d6806583cdea52ce9be1f17f3670057
Scott Taylor <rscott.taylor01@gmail.com>
2023-07-03 14:37:58 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(driverArray, string) {
+    return driverArray.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(driverArray, string) {
+    return driverArray.filter(function(driver) {
+        return driver.toLowerCase().startsWith(string.toLowerCase())
+    })
+}
+
+function matchName (objArray, string) {
+    return objArray.filter(function(driver) {
+        return driver.name === string;
+    })
+}
